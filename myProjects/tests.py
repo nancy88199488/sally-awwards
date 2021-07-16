c15b9fdd1a10ca2b6909f9f48f69cca3dd62505e
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-16 14:34:18 +0300

    added the test file

--- myProjects/tests.py
+++ myProjects/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from .models import Profile,MyProjects,Rating
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.user= User(username = 'salome',password = 'mnbvcxzasdfghjkl')
+        self.user = Profile(user = self.user,user = 1,Bio = 'tests',image = 'test.jpg',date_craeted='jan,28.2021')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user,Profile))
+
+    def test_save_profile(self):
+        Profile.save_profile(self)
+        all_profiles = Profile.objects.all()
+        self.assertTrue(len(all_profiles),0)
+
+    def test_delete_profile(self):
+        self.user.delete_profile()
+        all_profiles = Profile.objects.all()
+        self.assertEqual(len(all_profiles),0)
+
+class MyProjectsTestCase(TestCase):
+    def setUp(self):
+        self.new_post = myProjects(title = 'testT',image = 'test.jpg',description = 'testD',user = peris,projecturl = 'https://test.com',datecreated='Dec,01.2020')
+
+
+    def test_save_project(self):
+        self.new_post.save_project()
+        Images = Images.objects.all()
+        self.assertEqual(len(Images),1)
+
+    def test_delete_project(self):
+        self.new_post.delete_project()
+        Images = myProjects.objects.all()
+        self.assertEqual(len(Images),1)    
+
+class RatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='charles')
+        self.myProjects = myProjects.objects.create(id=1, title='test myProjects', photo='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', description='desc',
+                                        user=self.user, url='http://ur.coml')
+        self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, myProjects=self.myProjects)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
+
+    def test_save_rating(self):
+        self.rating.save_rating()
+        rating = Rating.objects.all()
+        self.assertTrue(len(rating) > 0)
+
+    def test_get_post_rating(self, id):
+        self.rating.save()
+        rating = Rating.get_ratings(post_id=id)
+        self.assertTrue(len(rating) == 1)        
