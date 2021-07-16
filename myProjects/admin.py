7cc0322f8d7f56941c04f6a0ccf623eaee6fad99
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-16 15:34:13 +0300

    registered the models and updated the app urls file

--- myProjects/admin.py
+++ myProjects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MyProjects, Profile, Rating,
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(MyProjects)
+admin.site.register(Rating)
--- myProjects/urls.py
+++ myProjects/urls.py
@@ -1,18 +1,25 @@
-from django.urls import path, include
+from django.urls import include,path
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('signup/', views.signup, name='signup'),
-    path('search/', views.search_profile, name='search'),
-    path('account/', include('django.contrib.auth.urls')),
-#     path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
-#     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-#     path('reset<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-#     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='reset_password_complete'),
-#     path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html'), name='password')
+    path('',views.index, name = 'index'),
+    path('register/',views.register, name='registration'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('search/', views.searchprofile, name='search'),
+    path('newproject/',views.addProject,name = 'project'),
+    path('profile/<id>/',views.profile,name = 'profile'),
+    path('editprofile/',views.editprofile,name = 'editprofile'), 
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('api/profile',views.ProfileList.as_view()),
+    path('api/projects',views.ProjectList.as_view()),
+    path('projects/<id>/',views.projects,name = 'projects'),
+    path(r'ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('rate/<id>/',views.rate,name = 'rate')
+
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
