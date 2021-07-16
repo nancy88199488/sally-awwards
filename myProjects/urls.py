f14a284f3b9f6ad4e38cc8d29a5f8116eb679fe1
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-16 15:49:44 +0300

    updated the app urls files

--- myProjects/urls.py
+++ myProjects/urls.py
@@ -6,18 +6,24 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.index, name = 'index'),
-    path('register/',views.register, name='registration'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('signup/',views.register, name='registration'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('search/', views.searchprofile, name='search'),
     path('newproject/',views.addProject,name = 'project'),
     path('profile/<id>/',views.profile,name = 'profile'),
     path('editprofile/',views.editprofile,name = 'editprofile'), 
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('api/profile',views.ProfileList.as_view()),
     path('api/projects',views.ProjectList.as_view()),
     path('projects/<id>/',views.projects,name = 'projects'),
-    path(r'ratings/', include('star_ratings.urls', namespace='ratings')),
-    path('rate/<id>/',views.rate,name = 'rate')
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('rate/<id>/',views.rate,name = 'rate'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='reset_password_complete'),
+    path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html'), name='password')
+
 
 ]
 
