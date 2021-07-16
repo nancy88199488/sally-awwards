5711fc8cdade06707cc4ac937359db8de1bf4d5b
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-16 12:08:06 +0300

    added the class myProjects

--- myProjects/models.py
+++ myProjects/models.py
@@ -21,3 +21,30 @@ class Profile(models.Model):
     @classmethod
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
+
+
+
+class MyProjects(models.Model):
+    title = models.CharField(max_length=155)
+    projecturl = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    image = CloudinaryField("myprojects")
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()        
