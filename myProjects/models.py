372097c8c632436548a55705eaba143890dac4b1
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-16 11:20:05 +0300

    modified the models and urls file

--- myProjects/models.py
+++ myProjects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+    profile_image= CloudinaryField('images')
+    Bio = models.CharField(max_length=30)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    datecreated= models.DateField(auto_now_add=True )
+
+    def __str__(self):
+        return self.user.username
+ 
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()    
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
--- myProjects/urls.py
+++ myProjects/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path,include,re_path
-from .import views 
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('search/', views.search_profile, name='search'),
+    path('account/', include('django.contrib.auth.urls')),
+#     path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+#     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+#     path('reset<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+#     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='reset_password_complete'),
+#     path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html'), name='password')
 ]
